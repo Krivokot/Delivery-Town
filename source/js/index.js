6d3908a765620e95434ed3679d2633b24b48505d
Krivokot <37597505+Krivokot@users.noreply.github.com>
2020-10-21 18:06:29 +0300

    закончил мобильную версию

--- source/js/index.js
+++ source/js/index.js
@@ -1,26 +1,14 @@
-// var button = document.querySelector(".page-header__button");
-// var popup = document.querySelector(".main-form");
-
-// button.addEventListener("click", function(evt) {
-//     evt.preventDefault();
-//     popup.classList.toggle("main-form--open");
-//   });
-
-// $('.page-header__button').on('click', function() {
-//     $('.main-form').toggleClass('.main-form--open');
-// });
-
 let form = document.querySelector('.main-form');
 let button = document.querySelector('.page-header__button');
 
 button.onclick = function() {
-    if (form) {
+    if (form.className == 'main-form') {
         form.classList.remove('main-form');
         form.classList.add('main-form--open');
+    } else {
+        form.classList.remove('main-form--open');
+        form.classList.add('main-form');
     }
-    // form.classList.remove('main-form');
-    // form.classList.add('main-form--open');
-    // form.classList.toggle('main-form');
 };
 
 
