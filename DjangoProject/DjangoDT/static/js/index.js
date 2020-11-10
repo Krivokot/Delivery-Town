bb98ce65749a56506ca17059a114b7c2dd8bc8e1
marchell93 <marchell93@mail.ru>
2020-11-10 17:30:23 +0300

    Последний билд 10.11.2020, форма отправки работает
    Последний билд 10.11.2020, форма отправки работает

---
+++ DjangoProject/DjangoDT/static/js/index.js
@@ -0,0 +1,42 @@
+let form = document.querySelector('.main-form');
+let button = document.querySelector('.page-header__button');
+let submit = document.querySelector('.main-form-fieldset__item-button');
+let order = document.querySelector('.page-header__order');
+// document.querySelector(".main-form").style.animationPlayState = "play";
+if (window.matchMedia('(min-width: 768px)').matches) {
+    form.classList.remove('main-form');
+    form.classList.add('main-form--open');
+    form.style.animationDelay = "1s";
+    form.style.animationFillMode = "backwards";
+};
+
+button.onclick = function() {
+    if (form.className == 'main-form') {
+        form.classList.remove('main-form');
+        form.classList.add('main-form--open');
+        order.style.visibility = "hidden";
+    } else {
+        form.classList.remove('main-form--open');
+        form.classList.add('main-form');
+        form.style.animationFillMode = "forwards";
+        form.style.visibility = "visible";
+        order.style.visibility = "visible";
+    }
+};
+
+submit.onclick = function() {
+    submit.disabled=true;
+};
+
+function loop() {
+    $(button)
+        .delay(3000)
+        .animate({marginLeft: '+=20px'}, 100)
+        .animate({marginRight: '+=40px'}, 100)
+        .animate({marginRight: '-=20px'}, 100,
+        function () {
+            loop();
+        })
+};
+
+loop();
