05778295519d5bbe9fa186abbd04c75d229648fa
marchell93 <marchell93@mail.ru>
2020-11-13 15:59:42 +0300

    Поменял почту и скрыл пароль от почты
    Поменял почту и скрыл пароль от почты

--- DjangoProject/DjangoDT/DjangoDT/settings.py
+++ DjangoProject/DjangoDT/DjangoDT/settings.py
@@ -129,6 +129,6 @@ STATICFILES_DIRS = [
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'marchellopatrioti@gmail.com'
-EMAIL_HOST_PASSWORD = 'mk5647382910'
+EMAIL_HOST_PASSWORD = '***********'
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
