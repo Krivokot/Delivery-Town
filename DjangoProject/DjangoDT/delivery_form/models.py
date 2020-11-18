b805cdf57bcd7a4fbd662f9ed62bdfc43ec5c7b9
marchell93 <marchell93@mail.ru>
2020-11-18 19:04:26 +0300

    Изменено поле price с CharField на DecimalField
    Изменено поле price с CharField на DecimalField

--- DjangoProject/DjangoDT/delivery_form/models.py
+++ DjangoProject/DjangoDT/delivery_form/models.py
@@ -12,7 +12,7 @@ class DeliveryForm(models.Model):
     address = models.CharField(max_length=500, verbose_name='Адрес')
     payment = models.ForeignKey('Payment', on_delete=models.PROTECT, verbose_name='Оплата')
     personal_info = models.BooleanField(default=True,  verbose_name='Принимаю условия обработки персональных данных ')
-    price = models.CharField(max_length=5, verbose_name='Цена')
+    price = models.DecimalField(max_digits=9, decimal_places=0, verbose_name='Цена')
 
     def __str__(self):
         return f'Заказ № {self.number}'
